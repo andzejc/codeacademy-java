6be0c027422558a2373fa977bc615ec7b9c5fe2b
Pavel Vrublevskij <pavelvrublevskij@gmail.com>
2022-01-17 19:56:02 +0200

    [04 methods] - add method without parameter and without return

--- lession/methods/src/main/java/lt/codeacademy/App.java
+++ lession/methods/src/main/java/lt/codeacademy/App.java
@@ -1,13 +1,17 @@
 package lt.codeacademy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Vienas");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Du");
+        }
+        System.out.println("Trys");
+        printSimpleText();
+    }
+
+    public static void printSimpleText() {
+        System.out.println("Keturi");
     }
 }
