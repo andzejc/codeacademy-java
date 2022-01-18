0eef523ad8bcd9078d0e5b686bb93ed0e7006973
Pavel Vrublevskij <pavelvrublevskij@gmail.com>
2022-01-18 21:32:30 +0200

    [05 collections] add Collection Map task4 solution

--- lession/collections/src/main/java/lt/codeacademy/Main.java
+++ lession/collections/src/main/java/lt/codeacademy/Main.java
@@ -6,8 +6,11 @@ public class Main {
 //        SimpleArrayExample simpleArrayExample = new SimpleArrayExample();
 //        simpleArrayExample.example();
 
-        ArrayListExample arrayListExample = new ArrayListExample();
+//        ArrayListExample arrayListExample = new ArrayListExample();
 //        arrayListExample.example();
-        arrayListExample.task1();
+//        arrayListExample.task1();
+
+        HashMapExample mapExample = new HashMapExample();
+        mapExample.task4();
     }
 }
