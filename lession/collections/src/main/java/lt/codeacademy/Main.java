85bef1915465f2c3bcc30db7d9b19932cd03b110
Pavel Vrublevskij <pavelvrublevskij@gmail.com>
2022-01-18 18:38:05 +0200

    [05 collections] remove simple array example code

--- lession/collections/src/main/java/lt/codeacademy/Main.java
+++ lession/collections/src/main/java/lt/codeacademy/Main.java
@@ -1,26 +1,10 @@
 package lt.codeacademy;
 
-import java.util.Arrays;
-
 import static lt.codeacademy.ConsolePrinter.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] digits = new int[2];
-        digits[0] = 1;
-        digits[1] = 2;
-//        digits[2] = 2;  negalimas
 
-        for (int i = 0; i < digits.length; i++) {
-            print(digits[i]);
-        }
-
-        //foreach
-        for (int digit: digits) {
-            print(digit);
-        }
-
-        print(Arrays.toString(digits));
     }
 }
