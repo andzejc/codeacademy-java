a0e6ebe52726d350ea9b778f5d25f8fd6cc47661
kiminiuslt <kiminiuslt@gmail.com>
2022-01-20 21:52:46 +0300

    Enum gives back full country name and returning via method.

--- lession/java-lesson/src/main/java/lt/codeacademy/Country.java
+++ lession/java-lesson/src/main/java/lt/codeacademy/Country.java
@@ -1,8 +1,15 @@
 package lt.codeacademy;
 
 public enum Country {
-    LT, LV, EE, PL;
-   private void showLocale(){
+    LITHUANIA("LT"), ESTONIA("ES"), LATVIA("LV"), POLAND("PL");
+    private final String local;
 
+    Country(String local) {
+        this.local = local;
     }
+
+    public String showLocal() {
+        return local;
+    }
+
 }
--- lession/java-lesson/src/main/java/lt/codeacademy/Main.java
+++ lession/java-lesson/src/main/java/lt/codeacademy/Main.java
@@ -3,9 +3,9 @@ package lt.codeacademy;
 public class Main {
     public static void main(String[] args) {
 
-        Adress adress1 = new Adress("Vilnius", "Kareiviu", 23, 15, Country.LT);
-        Adress adress2 = new Adress("Poznań", "Matyi", 2, 30, Country.PL);
-        Adress adress3 = new Adress("Daugavpils", "Liginišķu iela", 50, 1, Country.LV);
+        Adress adress1 = new Adress("Vilnius", "Kareiviu", 23, 15, Country.LITHUANIA);
+        Adress adress2 = new Adress("Poznań", "Matyi", 2, 30, Country.POLAND);
+        Adress adress3 = new Adress("Daugavpils", "Liginišķu iela", 50, 1, Country.ESTONIA);
 
         System.out.println(adress1.showAdderess());
         System.out.println(adress2.showAdderess());
