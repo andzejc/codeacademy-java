e1201b3e7ad434a908744ee299814b3a424d26dc
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-20 21:01:16 +0200

    Add address class

---
+++ lession/java-lesson/src/main/java/lt/codeacademy/Adresas.java
@@ -0,0 +1,19 @@
+package lt.codeacademy;
+
+public class Adresas {
+    private String miestas;
+    private String gatve;
+    private int namoNumeris;
+    private int butoNumeris;
+
+    public Adresas(String miestas, String gatve, int namoNumeris, int butoNumeris){
+        this.miestas = miestas;
+        this.gatve = gatve;
+        this.namoNumeris = namoNumeris;
+        this.butoNumeris = butoNumeris;
+    }
+
+    public void showAddress(){
+        System.out.printf("Miestas: %s, gatve: %s, namo numeris: %d, buto numeris: %d", miestas, gatve, namoNumeris, butoNumeris);
+    }
+}
--- lession/java-lesson/src/main/java/lt/codeacademy/Main.java
+++ lession/java-lesson/src/main/java/lt/codeacademy/Main.java
@@ -2,6 +2,8 @@ package lt.codeacademy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Adresas a1 = new Adresas("Vilnius", "Geležinio vilko", 26, 5);
+        Adresas a2 = new Adresas("Kaunas", "Taikos prospektas", 40, 11);
+        a1.showAddress();
     }
 }
