666eaf1a1182fa0da39dd6a0c29424ef84dd2830
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-20 21:13:53 +0200

    Added getters and setters in Adresas

--- lession/java-lesson/src/main/java/lt/codeacademy/Adresas.java
+++ lession/java-lesson/src/main/java/lt/codeacademy/Adresas.java
@@ -16,4 +16,36 @@ public class Adresas {
     public void showAddress(){
         System.out.printf("Miestas: %s, gatve: %s, namo numeris: %d, buto numeris: %d\n", miestas, gatve, namoNumeris, butoNumeris);
     }
+
+    public String getMiestas() {
+        return miestas;
+    }
+
+    public void setMiestas(String miestas) {
+        this.miestas = miestas;
+    }
+
+    public String getGatve() {
+        return gatve;
+    }
+
+    public void setGatve(String gatve) {
+        this.gatve = gatve;
+    }
+
+    public int getNamoNumeris() {
+        return namoNumeris;
+    }
+
+    public void setNamoNumeris(int namoNumeris) {
+        this.namoNumeris = namoNumeris;
+    }
+
+    public int getButoNumeris() {
+        return butoNumeris;
+    }
+
+    public void setButoNumeris(int butoNumeris) {
+        this.butoNumeris = butoNumeris;
+    }
 }
