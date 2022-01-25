670f010c453a28b4aa53a5f53c3dfd83133cf4c4
Pavel Vrublevskij <pavelvrublevskij@gmail.com>
2022-01-25 18:37:44 +0200

    [9 inheritance] use parent class default constructor

--- lesson/inheritance/src/main/java/lt/codeacademy/Inheritance.java
+++ lesson/inheritance/src/main/java/lt/codeacademy/Inheritance.java
@@ -1,8 +1,14 @@
 package lt.codeacademy;
 
+import lt.codeacademy.model.Student;
+
+import java.util.Scanner;
+
 public class Inheritance {
 
-    public void init() {
+    private Scanner scanner = new Scanner(System.in);
 
+    public void init() {
+        Student student = new Student();
     }
 }
--- lesson/inheritance/src/main/java/lt/codeacademy/model/Person.java
+++ lesson/inheritance/src/main/java/lt/codeacademy/model/Person.java
@@ -4,4 +4,14 @@ public class Person {
 
     private String name;
     private String surname;
+
+    public Person() {
+        System.out.println("Person is created from default constructor");
+    }
+
+    public Person(String name, String surname) {
+        this();
+        this.name = name;
+        this.surname = surname;
+    }
 }
