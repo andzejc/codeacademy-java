e810b1ceba8672e2eb2cd0ba4ad07c04c31ed1ff
Pavel Vrublevskij <pavelvrublevskij@gmail.com>
2022-01-31 19:24:21 +0200

    [12 polymorphism] add polymorphism example

--- lession/polimorfism/calculator/src/main/java/lt/codeacademy/CalculatorApplication.java
+++ lession/polimorfism/calculator/src/main/java/lt/codeacademy/CalculatorApplication.java
@@ -9,7 +9,12 @@ public class CalculatorApplication {
     public void doCalc() {
         Calculator sumCalculator = new SumCalculator(2, 3);
         Calculator substractCalculator = new SubstractCalculator(3, 2);
-        System.out.println(sumCalculator.doOperationWithTwoDigits());
-        System.out.println(substractCalculator.doOperationWithTwoDigits());
+
+        doCalculatorOperation(sumCalculator);
+        doCalculatorOperation(substractCalculator);
+    }
+
+    public void doCalculatorOperation(Calculator calculator) {
+        System.out.println(calculator.doOperationWithTwoDigits());
     }
 }
