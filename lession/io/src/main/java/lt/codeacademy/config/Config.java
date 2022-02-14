1015d9965cbd6e9c5b8b3eb890ff2c66521c3fe7
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-02-14 19:26:35 +0200

    [20 io] file path fix

--- lession/io/src/main/java/lt/codeacademy/config/Config.java
+++ lession/io/src/main/java/lt/codeacademy/config/Config.java
@@ -2,5 +2,5 @@ package lt.codeacademy.config;
 
 public class Config {
 
-    public static final String RESOURCE_FILE_PATH = "/home/pavel/code/ca/java101-4/codeacademy-java/lession/io/src/main/resources/";
+    public static final String RESOURCE_FILE_PATH = "lession/io/src/main/resources/";
 }
