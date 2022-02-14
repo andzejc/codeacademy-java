4a1b24df612e4b7962ef69595a930899d7e5a861
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-02-14 19:17:41 +0200

    [20 io] use simple way without close streams

--- lession/io/src/main/java/lt/codeacademy/App.java
+++ lession/io/src/main/java/lt/codeacademy/App.java
@@ -6,20 +6,10 @@ import static lt.codeacademy.config.Config.RESOURCE_FILE_PATH;
 
 public class App {
     public static void main(String[] args) {
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            try {
-                in = new FileInputStream(RESOURCE_FILE_PATH + "orig.txt");
-                out = new FileOutputStream(RESOURCE_FILE_PATH + "copy.txt");
-                for (int c; (c = in.read()) != -1; ) {
-                    out.write(c);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                in.close();
-                out.close();
+        try (InputStream in = new FileInputStream(RESOURCE_FILE_PATH + "orig.txt");
+             OutputStream out = new FileOutputStream(RESOURCE_FILE_PATH + "copy.txt")) {
+            for (int c; (c = in.read()) != -1; ) {
+                out.write(c);
             }
         } catch (IOException e) {
             e.printStackTrace();
