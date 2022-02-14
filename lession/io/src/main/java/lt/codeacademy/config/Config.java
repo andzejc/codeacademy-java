8fd2f6b53c039e04983aab6cf90ab638c7479a1b
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-02-14 19:13:14 +0200

    [20 io] use stream with try-catch-finally

---
+++ lession/io/src/main/java/lt/codeacademy/config/Config.java
@@ -0,0 +1,6 @@
+package lt.codeacademy.config;
+
+public class Config {
+
+    public static final String RESOURCE_FILE_PATH = "/home/pavel/code/ca/java101-4/codeacademy-java/lession/io/src/main/resources/";
+}
