c9a479877a1a8b107e3e6657a77d9086abd2b7c4
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-02-14 21:47:32 +0200

    [20 io] save and get list of person

--- lession/io/src/main/java/lt/codeacademy/InputOutputObjectExample.java
+++ lession/io/src/main/java/lt/codeacademy/InputOutputObjectExample.java
@@ -3,17 +3,23 @@ package lt.codeacademy;
 import lt.codeacademy.model.Person;
 import lt.codeacademy.utils.FileUtils;
 
+import java.util.List;
+
 public class InputOutputObjectExample {
 
     private static final String PERSON_FILE = "person.data";
 
     public static void init() {
-        FileUtils.writeToFile(
-                PERSON_FILE,
-                new Person("Antanas", "Antanaitis", 20)
+        List<Person> persons = List.of(
+            new Person("Antanas", "Antanaitis", 20),
+            new Person("Petras", "Antanaitis", 21),
+            new Person("Jonas", "Antanaitis", 22)
         );
+        FileUtils.writeToFile(PERSON_FILE, persons);
 
-        Person personObjFromFile = (Person) FileUtils.readFromFile(PERSON_FILE);
-        System.out.println(personObjFromFile.toString());
+        List<Person> personObjFromFile = (List<Person>) FileUtils.readFromFile(PERSON_FILE);
+        for (Person person : personObjFromFile) {
+            System.out.println(person.toString());
+        }
     }
 }
