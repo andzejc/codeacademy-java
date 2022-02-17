4edd0b3ccf8818d467415f24a785475e8c2f5d67
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-02-17 18:56:57 +0200

    [22 generics] difference with Generic<?> and Generic<Object>

--- lession/generics/src/main/java/inheritance/Inheritance.java
+++ lession/generics/src/main/java/inheritance/Inheritance.java
@@ -9,16 +9,24 @@ public class Inheritance {
         Generic<A> gA = new Generic<>();
         Generic<B> gB = new Generic<>();
         Generic<C> gC = new Generic<>();
+        Generic<Object> gObject = new Generic<>();
+
         doSomething(gA);
         doSomething(gB);
         doSomethingDownCast(gC);
         doSomethingDownCast(gB); // compile exception
         doSomethingDownCast(gA);
+
+        doSomethingObjectOnly(gObject);
+        doSomethingObjectOnly(gObject);
+        doSomethingObjectOnly(gA);  // compile exception
     }
 
     public static void doSomething(A obj) {}
 
     public static void doSomething(Generic<?> obj) {}
+
+    public static void doSomethingObjectOnly(Generic<Object> obj) {}
 
     public static void doSomethingDownCast(Generic<? super C> obj) {}
 
