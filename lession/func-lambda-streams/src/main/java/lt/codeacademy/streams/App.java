b6d86835335dc10f361ea3757771047b7277046f
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-02-22 19:15:15 +0200

    [23 func+lambda+stream] data types and different collections mapping + sorting

--- lession/func-lambda-streams/src/main/java/lt/codeacademy/streams/App.java
+++ lession/func-lambda-streams/src/main/java/lt/codeacademy/streams/App.java
@@ -1,6 +1,7 @@
 package lt.codeacademy.streams;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class App {
@@ -40,5 +41,16 @@ public class App {
                 .collect(Collectors.toList());
 
         stringNumbersFiltered.forEach(System.out::println);
+
+        // Stream change data types
+        System.out.println("------- Set<Integer> to List<Long> -----------");
+        Set<Integer> num1 = Set.of(1, 2, 3, 4);
+        List<Long> num1Long = num1.stream()
+                .map(value -> value.longValue())
+                .map(value -> value * 2)
+                .sorted()  // without sorted result will be randomly sorted
+                .collect(Collectors.toList());
+        num1Long.forEach(System.out::println);
+
     }
 }
