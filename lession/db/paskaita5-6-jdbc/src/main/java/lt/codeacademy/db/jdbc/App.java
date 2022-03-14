dba47e535ccdafe2edc98e743df858161e6a653b
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-03-14 21:13:30 +0200

    [db] [5 paskaita] access differently values from properties

--- lession/db/paskaita5-6-jdbc/src/main/java/lt/codeacademy/db/jdbc/App.java
+++ lession/db/paskaita5-6-jdbc/src/main/java/lt/codeacademy/db/jdbc/App.java
@@ -2,20 +2,35 @@ package lt.codeacademy.db.jdbc;
 
 import lt.codeacademy.db.jdbc.utils.FileUtil;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
+        /**
+         * Alternative usage
+         */
+/*        try {
+            InputStream inuStream = App.class.getClassLoader().getResourceAsStream("database.properties");
+            Properties props = new Properties();
+            props.load(inuStream);
+            String url = props.getProperty("database.jdbc.url");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
         List<String> properties = new FileUtil().readFrom("database.properties");
 
         try {
             Connection connection = DriverManager.getConnection(
                     properties.get(0).split("=")[1],
                     properties.get(1).split("=")[1],
-                    properties.get(2).split("=")[1]);
+                    System.getProperty("db.postgres.pass"));
         } catch (SQLException e) {
             throw new RuntimeException("Connection into database has a problem: " + e.getMessage());
         }
