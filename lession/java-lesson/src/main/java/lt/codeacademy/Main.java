d0de0e9ad3f082f39cd392605bb9003ee2aa63cc
Andzej <andzej.cernevic@gmail.com>
2022-03-16 08:49:03 +0200

    add preparestatment

--- lession/java-lesson/src/main/java/lt/codeacademy/Main.java
+++ lession/java-lesson/src/main/java/lt/codeacademy/Main.java
@@ -2,6 +2,7 @@ package lt.codeacademy;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,12 +15,25 @@ public class Main {
     Class.forName("org.postgresql.Driver");
     try (Connection connection = DriverManager.getConnection(conectionUrl, userName, password);
         Statement statement = connection.createStatement()) {
-      statement.executeUpdate("INSERT INTO author(id, name) VALUES (5, 'Jonas')");
+//      statement.executeUpdate("INSERT INTO author(id, name) VALUES (5, 'Jonas')");
       ResultSet resultSet = statement.executeQuery("select * from author");
       while (resultSet.next()) {
-        System.out.println(resultSet.getInt("id"));
-        System.out.println(resultSet.getString("name"));
-        System.out.println("--------------------------------");
+//        System.out.println(resultSet.getInt("id"));
+//        System.out.println(resultSet.getString("name"));
+//        System.out.println("--------------------------------");
+
+
+        // kad isvengti sql injection mes turetumeme naudoti ne Statement o PreapredStatement klase ir padavineti
+        //parametrus
+        int userId = 2;
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from author where id = ?");
+        preparedStatement.setInt(1, userId);
+        ResultSet resultSet1 = preparedStatement.executeQuery();
+        while (resultSet1.next()){
+          System.out.println(resultSet1.getInt("id"));
+          System.out.println(resultSet1.getString("name"));
+          System.out.println("--------------------------------");
+        }
       }
 
     } catch (SQLException e) {
