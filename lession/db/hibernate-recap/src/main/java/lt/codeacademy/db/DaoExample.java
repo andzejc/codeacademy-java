381022a85ba95dada6635dfa0d4a6bef245fca60
pavel.vrublevskij <pavel.vrublevskij@gmail.com>
2022-03-22 21:56:15 +0200

    [db] [9 paskaita hibernate recap] dao example in crud (v2)

---
+++ lession/db/hibernate-recap/src/main/java/lt/codeacademy/db/DaoExample.java
@@ -0,0 +1,41 @@
+package lt.codeacademy.db;
+
+import lt.codeacademy.db.dao.EmployeeDao;
+import lt.codeacademy.db.entity.EmployeeEntity;
+import lt.codeacademy.db.helper.DaoManager;
+import org.hibernate.Session;
+
+import java.util.Optional;
+
+public class DaoExample {
+
+    private static EmployeeDao employeeDao = null;
+
+    public static void main(String[] args) {
+        try (Session session = DaoManager.getSession()) {
+            employeeDao = new EmployeeDao(session);
+            createNewEmployee(session);
+            updateCurrentEmployee(session);
+            deleteCurrentEmployee(session);
+        }
+    }
+
+    private static void createNewEmployee(Session session) {
+        EmployeeEntity employee = EmployeeEntity.builder()
+                .name("Alma")
+                .build();
+        employeeDao.saveOrUpdate(employee);
+    }
+
+    private static void updateCurrentEmployee(Session session) {
+        Optional<EmployeeEntity> employee = new EmployeeDao(session).findById(2L);
+
+        if (employee.isPresent()) {
+            employeeDao.saveOrUpdate(employee.get());
+        }
+    }
+
+    private static void deleteCurrentEmployee(Session session) {
+        employeeDao.delete(1L);
+    }
+}
--- lession/db/hibernate-recap/src/main/java/lt/codeacademy/db/dao/EmployeeDao.java
+++ lession/db/hibernate-recap/src/main/java/lt/codeacademy/db/dao/EmployeeDao.java
@@ -5,6 +5,7 @@ import lt.codeacademy.db.entity.EmployeeEntity;
 import org.hibernate.Session;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class EmployeeDao {
@@ -15,5 +16,19 @@ public class EmployeeDao {
         return session
                 .createQuery("FROM EmployeeEntity", EmployeeEntity.class)
                 .getResultList();
+    }
+
+    public void saveOrUpdate(EmployeeEntity employee) {
+        session.saveOrUpdate(employee);
+    }
+
+    public Optional<EmployeeEntity> findById(long id) {
+        return Optional.ofNullable(session.get(EmployeeEntity.class, id));
+    }
+
+    public void delete(long id) {
+        session.beginTransaction();
+        findById(id).ifPresent(session::delete);
+        session.getTransaction().commit();
     }
 }
